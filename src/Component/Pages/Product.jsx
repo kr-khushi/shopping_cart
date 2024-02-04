2046fcffdf858fba844209f40568c2305fb69498
kr-khushi <khushi.p@knackroot.com>
2024-02-04 20:55:54 +0530

    tailwind css

--- src/Component/Pages/Product.jsx
+++ src/Component/Pages/Product.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import ProductCard from "../Extra_Component/ProductCard";
-import "../../style/product.css";
 import useFetchData from "../../hooks/useFetchData";
 import Pagination from "../Extra_Component/Pagination";
 
@@ -17,15 +16,6 @@ const Product = () => {
   const firstPostIndex = lastPostIndex - postPerPage;
   const currentposts = data.slice(firstPostIndex, lastPostIndex);
 
-  // useEffect(() => {
-  //   // Filter products based on the search query
-  //   setFilteredProducts(
-  //     currentposts.filter((product) =>
-  //       product.title.toLowerCase().includes(searchQuery.toLowerCase())
-  //     )
-  //   );
-
-  // }, [searchQuery, currentposts]);
 
 
   if (loading) {
@@ -37,25 +27,20 @@ const Product = () => {
 
   return (
     <>
-      <div className="search-bar">
-        <input
-          type="text"
-          placeholder="Search products..."
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
+      <div className="flex flex-col items-center m-12">
+        <div className="product-container grid grid-cols-1 md:grid-cols-4 gap-4 md:mx-12">
+          {data && data?.map((product) => (
+            <ProductCard key={product.id} {...product} />
+          ))}
+        </div>
+        <Pagination
+          className="mt-4"
+          totalPosts={80}
+          postPerPage={postPerPage}
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
         />
       </div>
-      <div className="product-container">
-        {data && data?.map((product) => (
-          <ProductCard key={product.id} {...product} />
-        ))}
-      </div>
-      <Pagination
-        totalPosts={80}
-        postPerPage={postPerPage}
-        currentPage={currentPage}
-        setCurrentPage={setCurrentPage}
-      />
     </>
   );
 };
